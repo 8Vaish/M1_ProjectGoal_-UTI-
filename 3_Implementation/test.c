915fc13f58273219427dcdae723c1391c07ee321
8Vaish <vaishnavibambal15@gmail.com>
2022-02-27 17:45:05 +0530

    newwork

---
+++ 3_Implementation/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "test.h"
+extern int que();
+void main()
+{
+    n=0;
+    printf("WELCOME TO THE GAME\nTest Your Intellect\n" "It contains10 QUESTIONS each carries 1000 cashrewards");
+    cashReward = 1000;
+    Marks=0;
+   que();
+
+}
---
+++ 3_Implementation/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+int i,a[10],n;
+int Marks;
+    int cashReward ;
+    int que();
+#endif
